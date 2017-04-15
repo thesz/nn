333e1108b1e6b1ad1b92f4641a5a8e4222018843
thesz <sergueyz@gmail.com>
2017-04-16 02:41:22 +0300

    Added new problem

---
+++ PenDigits.hs
@@ -0,0 +1,15 @@
+-- |PenDigits.hs
+--
+-- Training pendigits problem with our cool framework.
+--
+-- (C) 2017 Serguey Zefirov
+
+module PenDigits where
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import Text.Show.Pretty
+
+import E
+
